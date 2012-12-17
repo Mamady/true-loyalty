b8ec2df3991e33661e6a9186ddd6fbf73864f9f7
Mohammad <m@forouzani.com>
2012-12-17 10:10:21 +0000

    Fixed contact form.

--- src/TL/MainBundle/Controller/PageController.php
+++ src/TL/MainBundle/Controller/PageController.php
@@ -5,6 +5,8 @@ namespace TL\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class PageController extends Controller
 {
@@ -28,5 +30,40 @@ class PageController extends Controller
             return $this->render('TLMainBundle:Page:'.$page.'.html.twig');
         }
 
+    }
+
+
+    public function contactAction(Request $request)
+    {
+
+        $form = $this->createFormBuilder()
+            ->add('name', 'text')
+            ->add('email', 'email')
+            ->add('message', 'textarea')
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $params = $request->request->get('form');
+                $body = $params['message'];
+                $fromEmail = $params['email'];
+                $fromName = $params['name'];
+
+                $message = new \Swift_Message();
+                $message->setSubject('Contact Form Submission - '.time())
+                    ->setFrom(array($fromEmail => $fromName))
+                    ->setTo(array($this->container->getParameter('contact_email') => 'True Loyalty'))
+                    ->setBody('From: '.$fromName.' <br>'.$body)
+                    ->setContentType('text/html');
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->setFlash('success', 'Your message was sent successfully.');
+                return $this->redirect($this->generateUrl('TLMainBundle_page_contact'));
+            }
+        }
+
+        return $this->render('TLMainBundle:Page:contact.html.twig', array('form' => $form->createView()));
     }
 }
